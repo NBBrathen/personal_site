c90a42bc33f6f547290d7c495aeac1787e9f41fa
NBBrathen <noel@brathen.net>
2025-08-27 21:43:15 -0400

    Debug set to false

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#tzntfx85eupizug@mczj4m@*w-!gex!98gr0secf5-=zo5r@p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "noel-brady.com",
